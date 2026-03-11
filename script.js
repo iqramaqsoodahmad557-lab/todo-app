d0160fcce514d750557fff7fa49733a3c68c57f2
iqramaqsoodahmad557-lab <iqramaqsoodahmad557@gmail.com>
2026-03-12 01:49:42 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function addTask(){
+
+let input = document.getElementById("taskInput");
+
+let task = input.value;
+
+if(task === ""){
+alert("Please write a task");
+return;
+}
+
+let li = document.createElement("li");
+
+li.textContent = task;
+
+document.getElementById("taskList").appendChild(li);
+
+input.value = "";
+
+}
